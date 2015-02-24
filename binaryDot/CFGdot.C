9a61a026c00cb95a7484f4a722d8b41158a25419
David Alm <goliat.90@gmail.com>
2015-02-24 17:27:14 +0100

    Added debug flag to make file. Attempt to improve the graph generator, smaller graphs.

--- binaryDot/CFGdot.C
+++ binaryDot/CFGdot.C
@@ -12,27 +12,24 @@ int main( int argc, char * argv[] )
     // generates two lines of printout
     SgProject* project = frontend(argc,argv);
 
-    //Find the main function to reduce the cfg.
-    //SgFunctionDeclaration* main = SageInterface::findMain(project);
-
-    //retrieve the assembly interpretation from the main.
+    //retrieve the assembly interpretation.
     std::vector<SgAsmInterpretation*> test = SageInterface::querySubTree<SgAsmInterpretation>(project);
 
     //i have many assembler interpretations now. Should find relevant ones (main):
 
     //retrieve the assembler interpertation.
-    std::vector<SgAsmInterpretation*> interps = SageInterface::querySubTree<SgAsmInterpretation>(project);
+    //std::vector<SgAsmInterpretation*> interps = SageInterface::querySubTree<SgAsmInterpretation>(project);
 
     //Create CFG from the SgProject, code from binaryCFGTraversalTutorial.C
-    rose::BinaryAnalysis::ControlFlow cfg_analyzer;
-    rose::BinaryAnalysis::ControlFlow::Graph* cfg = new rose::BinaryAnalysis::ControlFlow::Graph;
+    //rose::BinaryAnalysis::ControlFlow cfg_analyzer;
+    //rose::BinaryAnalysis::ControlFlow::Graph* cfg = new rose::BinaryAnalysis::ControlFlow::Graph;
 
     //build the CFG, interps.back = last element in the vector.
-    cfg_analyzer.build_block_cfg_from_ast(interps.back() , *cfg);
+    //cfg_analyzer.build_block_cfg_from_ast(interps.back() , *cfg);
     //cfg_analyzer.build_block_cfg_from_ast(test.back(), *cfg);
 
     //call the graph maker function when i have a cfg.
-    BinaryDotGenerator(*cfg, "main" , "main.dot", false);
+    //BinaryDotGenerator(*cfg, "main" , "main.dot", false);
 
     //
     return 0;
