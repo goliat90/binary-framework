0c0625e58fc735056f8daecf02ef187226167374
David Alm <goliat.90@gmail.com>
2015-02-26 13:43:57 +0100

    A version that finds the SgAsmFunction object for main and only builds a CFG for it.

--- binaryDot/CFGdot.C
+++ binaryDot/CFGdot.C
@@ -14,6 +14,28 @@ int main( int argc, char * argv[] )
 
     //retrieve the assembly interpretation.
     std::vector<SgAsmInterpretation*> test = SageInterface::querySubTree<SgAsmInterpretation>(project);
+    
+    //see how many assembly interpertations we have.
+    std::cout << "SgAsmInterpertations: " << test.size() << "\n";
+
+    //find all the SgAsmFunction nodes in the SgAsmInterpretation
+    std::vector<SgAsmFunction*> test2 = SageInterface::querySubTree<SgAsmFunction>(test.back());
+
+    std::cout << "SgAsmFunctions: " << test2.size() << "\n";
+
+    //print out the function names.
+    SgAsmFunction* onlyMain;
+    int i = 0;
+    for (std::vector<SgAsmFunction*>::iterator iter = test2.begin(); iter != test2.end(); iter++) {
+        //print out the function name.
+        //std::cout << i << ": " << (*iter)->get_name() << "\n";
+        if ((*iter)->get_name() == "main") {
+            std::cout << "main found" << "\n";
+            onlyMain = *iter;
+            std::cout << onlyMain->get_name() << "\n";
+        }
+        i++;
+    }
 
     //i have many assembler interpretations now. Should find relevant ones (main):
 
@@ -21,15 +43,17 @@ int main( int argc, char * argv[] )
     //std::vector<SgAsmInterpretation*> interps = SageInterface::querySubTree<SgAsmInterpretation>(project);
 
     //Create CFG from the SgProject, code from binaryCFGTraversalTutorial.C
-    //rose::BinaryAnalysis::ControlFlow cfg_analyzer;
-    //rose::BinaryAnalysis::ControlFlow::Graph* cfg = new rose::BinaryAnalysis::ControlFlow::Graph;
+    rose::BinaryAnalysis::ControlFlow cfg_analyzer;
+    rose::BinaryAnalysis::ControlFlow::Graph* cfg = new rose::BinaryAnalysis::ControlFlow::Graph;
 
     //build the CFG, interps.back = last element in the vector.
     //cfg_analyzer.build_block_cfg_from_ast(interps.back() , *cfg);
     //cfg_analyzer.build_block_cfg_from_ast(test.back(), *cfg);
+    //cfg_analyzer.build_block_cfg_from_ast(test2.back() , *cfg);
+    cfg_analyzer.build_block_cfg_from_ast(onlyMain , *cfg);
 
     //call the graph maker function when i have a cfg.
-    //BinaryDotGenerator(*cfg, "main" , "main.dot", false);
+    BinaryDotGenerator(*cfg, "main" , "main.dot", true);
 
     //
     return 0;
