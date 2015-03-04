5e5126918836d016796ad0350aa2895ecb3fb9d7
David Alm <goliat.90@gmail.com>
2015-03-04 18:03:17 +0100

    Changed mips testcode to use argc and argv to avoid optimization removal.

--- binaryDot/mipsBinary/test.c
+++ binaryDot/mipsBinary/test.c
@@ -1,16 +1,23 @@
 
-int main()
+//test code takes ascii numbers, convert them and add them together.
+//the sum of the numbers plus argc is returned.
+int main(int argc, char **argv)
 {
-    // Declarations
-    int x;
-    int y;
-    int z;
+    //Declarations
+    int acc = 0;
+    int i = 0;
 
-    // assignments and calculations
-    x = 1;
-    y = 2;
-    z = x + y;
-    
-    return z;
+    //go through argv and convert and check the numbers to be valid.
+    for (i = 1; i < argc; i++) {
+        //Get the number and adjust it.
+        int charNum = argv[i][0];
+        charNum -= '0';
+        //check if it is a number, if it is add it otherwise ignore.
+        if (charNum >= 0 && charNum <= 9) {
+            acc += charNum;
+        }
+    }
+    //return the sum.
+    return (acc + argc);
 }
 
