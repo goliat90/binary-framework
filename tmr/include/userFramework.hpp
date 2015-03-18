ec31d2256738c1bbb39e8684cc383cf720e8802f
David Alm <goliat.90@gmail.com>
2015-03-18 17:24:29 +0100

    Renamed files in the tmr folder, removed readme.

---
+++ tmr/include/userFramework.hpp
@@ -0,0 +1,15 @@
+#ifndef USERFRAMEWORK_H
+#define USERFRAMEWORK_H
+
+/*
+This class is used by the user to define the behavior of transformations.
+It is passed to the framework which calls it during traversal of the binary.
+*/
+class userTransformer {
+    public:
+
+    private:
+
+};
+
+#endif
