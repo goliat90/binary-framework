6ddb8caca41ce127ad0f7f8df6c54c52030aa612
David Alm <goliat.90@gmail.com>
2015-03-19 18:37:25 +0100

    Simplified the mips code to make the binary simpler.

--- binaryDot/mipsBinary/test.c
+++ binaryDot/mipsBinary/test.c
@@ -3,21 +3,11 @@
 //the sum of the numbers plus argc is returned.
 int main(int argc, char **argv)
 {
-    //Declarations
-    int acc = 0;
-    int i = 0;
-
-    //go through argv and convert and check the numbers to be valid.
-    for (i = 1; i < argc; i++) {
-        //Get the number and adjust it.
-        int charNum = argv[i][0];
-        charNum -= '0';
-        //check if it is a number, if it is add it otherwise ignore.
-        if (charNum >= 0 && charNum <= 9) {
-            acc += charNum;
-        }
-    }
+    int a = 1;
+    int b = 2;
+    int c = a + b;
+    
     //return the sum.
-    return (acc + argc);
+    return c;
 }
 
