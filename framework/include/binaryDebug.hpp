7a2bcf062d9326998b0c12f9c69fec3008f4659c
David Alm <goliat.90@gmail.com>
2015-03-30 19:43:32 +0200

    Source files for debug functionality.

---
+++ framework/include/binaryDebug.hpp
@@ -0,0 +1,13 @@
+/* Debug functions for the framework such as print functions */
+
+#ifndef BINARYDEBUG_H
+#define BINARYDEBUG_H 
+
+/* Headers. */
+#include "rose.h"
+
+/* function declarations  */
+void printBasicBlockInstructions(SgAsmBlock*);
+
+
+#endif
---
+++ framework/src/binaryDebug.cpp
@@ -0,0 +1,18 @@
+/* Functions for debugging the framework  */
+
+/* header file */
+#include "binaryDebug.hpp"
+
+
+
+/* print the instructions contained in a basicblock */
+void printBasicBlockInstructions(SgAsmBlock* block) {
+    //get the list of instructions in the block.
+    SgAsmStatementPtrList* stmtlistPtr = block->get_statementList();
+    //iterate through the statement list and print.
+    for(SgAsmStatementPtrList::iterator instIter = stmtlistPtr->begin();
+        instIter != stmtlistPtr.end(); instIter++) {
+        //decode the instruction.
+    }
+}
+
