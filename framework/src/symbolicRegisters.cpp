6030950f993dbf8a6b3a7c98459ee0c847b4fc3d
David Alm <goliat.90@gmail.com>
2015-03-31 13:57:18 +0200

    Symbolic register code complete, for now.

--- framework/src/symbolicRegisters.cpp
+++ framework/src/symbolicRegisters.cpp
@@ -2,12 +2,14 @@
 
 /* Header file */
 #include "symbolicRegisters.hpp"
-#include <string>
+#include <sstream>
 
-/* Number counter for the symbolic regs */
-static int symbolicNumber = 0;
+/* Forward declaration */
+std::string generateRegName();
+
 /* map to connect the register expression object to the symbolic register. */
 static std::map<SgAsmDirectRegisterExpression*, std::string> symbolicRegisterMap;
+typedef std::pair<SgAsmDirectRegisterExpression*, std::string> mapPair;
 
 /* Create a symbolic register that can be used */
 SgAsmDirectRegisterExpression generateSymbolicRegister() {
@@ -15,13 +17,25 @@ SgAsmDirectRegisterExpression generateSymbolicRegister() {
     RegisterDescriptor rd = RegisterDescriptor(mips_regclass_gpr, 0, 0, 0); 
     /* Create the register expression. */
     SgAsmDirectRegisterExpression regExp = SgAsmDirectRegisterExpression(rd);
-    /* Create the symbolic register name */
-    //to_string will probably not work. need to add files to make.
-    //std::string symReg = "symReg_" + std::to_string(symbolicNumber);
+    /* Get a sym register name */
+    std::string regName = generateRegName();
     /* Insert the register to the map with associated register */
-    
+    symbolicRegisterMap.insert(mapPair(&regExp, regName));
     /* Return the register expression */ 
-    
+    return regExp;
+}
+
+/* Generates the symbolic register name */
+std::string generateRegName() {
+    /* Number counter for the symbolic regs */
+    static int symbolicNumber = 0;
+    /* Create the symbolic register name */
+    std::stringstream symReg;
+    symReg << "Sym_" << symbolicNumber;
+    /* Increment the number */
+    symbolicNumber++;
+    /* return string */
+    return symReg.str();
 }
 
 /* Checks if a registerexpression is symbolic or not. */
