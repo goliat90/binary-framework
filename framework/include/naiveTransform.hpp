7e5fd046b1cd9aa2659a87e0b617f924d19812c1
David Alm <goliat.90@gmail.com>
2015-05-05 15:15:51 +0200

    Naive transformation files added.

---
+++ framework/include/naiveTransform.hpp
@@ -0,0 +1,17 @@
+#ifndef NAIVETRANSFORM_H
+#define NAIVETRANSFORM_H
+/* 
+* Naive transformation that is lazy when creating an executable binary.
+* It will in a simple way do register allocation, not regarding performance
+* at all, only considering creating an correct binary.
+*/
+
+/* Includes */
+#include "rose.h"
+
+
+
+
+
+
+#endif
---
+++ framework/src/naiveTransform.cpp
@@ -0,0 +1,6 @@
+/* Naive transformation implementation.  */
+
+#include "naiveTransformatiom.hpp";
+
+
+
