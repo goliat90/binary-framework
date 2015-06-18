dbc6d506f4c9d48980c2f1803727fead31ce62d2
David Alm <goliat.90@gmail.com>
2015-06-18 15:03:21 +0200

    Made the mips test code complicated. Did this to force a decent cfg.

--- mipsCode/mipsSimpleAdd.c
+++ mipsCode/mipsSimpleAdd.c
@@ -3,7 +3,15 @@
 /* Simple addiction function that is transformed */
 int simpleAdditionTMR(int a, int b) {
     /* add the numbers together and return them */
-    int result = a + b;
+    int result = 0;
+    int ivar;
+    int i,j;
+
+    for (j = 0; j < a; j++) {
+        result++;
+    }
+    result += a * b;
+
     return result;
 }
 
@@ -12,8 +20,12 @@ int main(int argc, char** argv) {
     /* values to be added */
     int a = argv[0][0] - '0';
     int b = argv[1][0] - '0';
-    /* call the add function */
-    int c = simpleAdditionTMR(a, b);
+    int c;
 
+    /* call the add function */
+    if (argc > 3) {
+        c = simpleAdditionTMR(a, b);
+    } 
+    
     return c;
 }
