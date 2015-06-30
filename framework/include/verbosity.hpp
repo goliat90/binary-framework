1fd621e4f353383759b56ae39d4cb5a694aa6928
David Alm <goliat.90@gmail.com>
2015-06-30 23:28:15 +0200

    Header file to describe verbose printout level.

---
+++ framework/include/verbosity.hpp
@@ -0,0 +1,12 @@
+#ifndef VERBOSITY_H
+#define VERBOSITY_H
+/*  This is a header that defines levels of printout for the system. */
+
+enum verboseLevel{
+    SILENT = 0,
+    MINIMAL = 1,
+    DETAILED = 2,
+    DEBUGGING = 3
+};
+
+#endif
