6d8acb6ea0eb7dc82c2b7a3ec77402124e62b588
David Alm <goliat.90@gmail.com>
2015-07-02 18:17:34 +0200

    Function to replace registers used by linear scan.

--- framework/include/linearScan.hpp
+++ framework/include/linearScan.hpp
@@ -4,8 +4,15 @@
 
 /*  Includes    */
 #include "liveVariableAnalysis.hpp"
+#include "mipsISA.hpp"
+#include "symbolicRegisters.hpp"
 #include "cfgHandler.hpp"
 
+/*  Class for the linear scan algorithm. It does not use
+    all registers available, it uses t0-t7, t8,t9. To use them
+    it will replace all instances of these registers with symbolic
+    names before performing live-variable analysis.
+    A register will be used for spill. */
 class linearScanHandler {
     public:
     /*  Constructor */
@@ -26,7 +33,12 @@ class linearScanHandler {
     liveVariableAnalysisHandler* liveRangeHandler;
     /*  Container for free available registers to allocate */
     std::set<mipsRegisterName> freeRegisters;
-    
+    /*  Container mapping a physical register to a symbolic registers */
+    std::map<unsigned, mipsRegisterName> registerAllocationMap;
+
+    /*  Replace temporary registers with symbolic names before live-range analysis.
+        The registers used by linear scan is t0-7, t8,t9. */
+    void replaceHardRegisters();
     /*  Linear scan allocation function */
     void linearScanAllocation();
     /*  Expire old live interval */
@@ -35,8 +47,6 @@ class linearScanHandler {
     /*  Spill at interval */
     //TODO need to figure out the argument
     void spillAtInterval();
-    /*  Replace physical registers with symbolics before analysis and allocation */
-    void replaceHardRegisters();
 };
 
 #endif
--- framework/src/linearScan.cpp
+++ framework/src/linearScan.cpp
@@ -14,6 +14,22 @@ linearScanHandler::linearScanHandler(CFGhandler* passedCfgObject){
     cfgHandlerPtr = passedCfgObject;
     /*  Create the live variable analysis object */
     liveRangeHandler = new liveVariableAnalysisHandler(cfgHandlerPtr->getFunctionCFG());
+    /*  Initialize the freeRegister set */
+    /*  t0-t7 */
+    freeRegisters.insert(t0);
+    freeRegisters.insert(t1);
+    freeRegisters.insert(t2);
+    freeRegisters.insert(t3);
+    freeRegisters.insert(t4);
+    freeRegisters.insert(t5);
+    freeRegisters.insert(t6);
+    freeRegisters.insert(t7);
+    /*  t8-t9 */
+    freeRegisters.insert(t8);
+    freeRegisters.insert(t9);
+    /*  extra register to test. */
+    freeRegisters.insert(a2);
+    freeRegisters.insert(a3);
 }
 
 
@@ -27,12 +43,90 @@ void linearScanHandler::selectDebuging(bool mode) {
 
 /*  Applies the linear scan */
 void linearScanHandler::applyLinearScan() {
-    //TODO Might have to add here some kind of funciton to
-    //TODO free up the physical registers before live range analysis
+    /*  Go through the cfg and replace the register that linear scan will use. */
+    if (debuging) {
+        std::cout << "Replacing physical registers used by linear scan." << std::endl;
+    }
+    replaceHardRegisters();
 
     /*  Get live-range analysis done before performing register allocation. */
+    if (debuging) {
+        std::cout << "Starting the live-range analysis." << std::endl;
+    }
     liveRangeHandler->performLiveRangeAnalysis();
 
+}
+
+/*  Replace the registers used by linear scan with symbolic names.
+    Each physical register will be replaced with one symbolic register that
+    will be used in the whole CFG. This could be considered a greedy method
+    since it will result in long live ranges for these registers. */
+void linearScanHandler::replaceHardRegisters() {
+    /*  mapping between physical register and symbolic name */
+    std::map<mipsRegisterName, registerStruct> physicalToSymbolic;
+    /*  CFG pointer */
+    CFG* functionCFG = cfgHandlerPtr->getFunctionCFG();
+    /*  Iterate through the vertices */
+    for(std::pair<CFGVIter, CFGVIter> vertPair = vertices(*functionCFG);
+        vertPair.first != vertPair.second; ++vertPair.first) {
+        /*  Get the block pointer */
+        SgAsmBlock* block = get(boost::vertex_name, *functionCFG, *vertPair.first);
+        /*  Retrieve the statement list */
+        SgAsmStatementPtrList& stmtList = block->get_statementList();
+        /*  Go through the statement list and look for the specific registers */
+        for(SgAsmStatementPtrList::iterator stmtIter = stmtList.begin();
+            stmtIter != stmtList.end(); ++stmtIter) {
+            /*  Verify that the statement is a mips instruction */
+            if (V_SgAsmMipsInstruction == (*stmtIter)->variantT()) {
+                /*  Cast to mips instruction pointer */
+                SgAsmMipsInstruction* mips = isSgAsmMipsInstruction(*stmtIter);
+                /*  Get the operand list and check for instructions. */
+                SgAsmExpressionPtrList& opList = mips->get_operandList()->get_operands(); 
+                /*  Go through the operands */
+                for(int i = 0; i < opList.size(); i++) {
+                    /*  Get pointer to the expression */
+                    SgAsmExpression* expr = opList.at(i);
+                    /*  Check if the expression is a register */
+                    if (V_SgAsmDirectRegisterExpression == expr->variantT()) {
+                        /*  Decode the register */
+                        registerStruct reg = decodeRegister(expr);
+                        /*  Determine if the register should be replaced or not.
+                            Also check if it has been replaced already. */
+                        if (1 == freeRegisters.count(reg.regName)) {
+                            /*  The register should be replaced, check if it has a symbolic or not */
+                            if (0 == physicalToSymbolic.count(reg.regName)) {
+                                /*  First time it has been encountered, generate a symbolic */
+                                registerStruct symReg = generateSymbolicRegister();
+                                /*  Debug printout */
+                                if (debuging) {
+                                    std::cout << "Replaced a register with sym_" << std::dec
+                                                << symReg.symbolicNumber << " saved to map." << std::endl;
+                                }
+                                /*  Insert values into the map */
+                                physicalToSymbolic.insert(std::pair<mipsRegisterName, registerStruct>(reg.regName, symReg));
+                                /*  Build the register expression */
+                                SgAsmDirectRegisterExpression* DRE = buildRegister(symReg);
+                                /*  Replace the register expression with the new expression. */
+                                opList.at(i) = DRE;
+                            } else {
+                                /*  The register should be replaced, it has been replaced before. */
+                                registerStruct usedSymReg = physicalToSymbolic.find(reg.regName)->second;
+                                /*  Debug printout */
+                                if (debuging) {
+                                    std::cout << "Replaced a register with sym_" << std::dec
+                                                << usedSymReg.symbolicNumber << std::endl;
+                                }
+                                /*  Get the register expression */
+                                SgAsmDirectRegisterExpression* usedDRE = buildRegister(usedSymReg);
+                                /*  Replace the expression. */
+                                opList.at(i) = usedDRE;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 
