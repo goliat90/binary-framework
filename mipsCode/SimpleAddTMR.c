48395c7c39a2b7968b5ae22e9476c66581ec3885
David Alm <goliat.90@gmail.com>
2015-09-01 17:39:36 +0200

    Start on changing the structure of testcode.

---
+++ mipsCode/SimpleAddTMR.c
@@ -0,0 +1,22 @@
+/*  Test code consisting of main calling a simple add function. */
+
+#include "testHeader.h"
+
+/* Simple addiction function that is transformed */
+int simpleAddTMR(int a, int b) {
+    /* add the numbers together and return them */
+    int result = 0;
+    int j;
+
+    for (j = 0; j < a; j++) {
+        result++;
+        if (result < b) {
+            result++;
+        }
+    }
+
+    if (a < b) {
+        result += a * b;
+    }
+    return result;
+}
---
+++ mipsCode/testHeader.h
@@ -0,0 +1,12 @@
+/*  This header contains the names of functions
+    that are being transformed. */
+
+#ifndef TEST_H
+#define TEST_H
+
+/*  Simpel addition function. */
+int simpleAddTMR(int, int);
+
+
+
+#endif
