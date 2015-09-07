a3e1e62cca6346d212b25c499152212570851698
David Alm <goliat.90@gmail.com>
2015-09-07 14:36:36 +0200

    Fixed test script.

--- mipsCode/testScript.sh
+++ mipsCode/testScript.sh
@@ -5,13 +5,32 @@
 
 BINS=./binaries/*
 
-echo "Testing all binaries."
+echo -e "Testing all binaries in naive mode then optimized mode.\n"
 #how about adding opt or naive as variable and the same with debugging.
 for bin in $BINS
 do
     FUNC=$(basename $bin .out)
-    echo "Testing ${bin##*/} and transforming function ${FUNC}" 
-    ../tmr/userRewriter.out $bin $FUNC 0 || exit
-
-# catch the failure and continue with a test.
+    echo "Testing naive mode on ${bin##*/} and transforming function ${FUNC}" 
+    ../tmr/userRewriter.out "$bin" "$FUNC" "0" > /dev/null 2>&1
+    RETVAL=$?
+    if [ ! "$RETVAL" -eq "0" ]
+    then 
+        # the Return value is a failure value.
+        echo -e "Naive transform on $bin, function $FUNC, failed.\n" 
+    else 
+        echo -e "Naive transformation on ${FUNC} in binary ${bin##*/} successful\n" 
+    fi
+    # Test the optimized version for the same binary
+    echo "Testing optimized mode on ${bin##*/} and transforming function ${FUNC}" 
+    ../tmr/userRewriter.out "$bin" "$FUNC" "1" > /dev/null 2>&1
+    if [ ! "$RETVAL" -eq "0" ]
+    then 
+        # the Return value is a failure value.
+        echo -e "Opt transform on function $FUNC in binary $bin failed.\n" 
+    else
+        echo -e "Opt transformation on ${FUNC} in binary ${bin##*/} successful\n" 
+    fi
 done
+echo -e "Tests finished\n"
+
+
